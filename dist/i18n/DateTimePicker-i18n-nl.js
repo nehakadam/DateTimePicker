30bfa078c4d696934230f9b3b660e49a9110996c
Bernardo <bhulsman@users.noreply.github.com>
2015-10-05 15:37:26 +0200

    Added Dutch translation.
    Translating the titles and buttons is not very straightforward, but this 'sounds' very natural. In the Dutch language; month names and day names are written starting with a lower letter.

---
+++ dist/i18n/DateTimePicker-i18n-nl.js
@@ -0,0 +1,40 @@
+/* ----------------------------------------------------------------------------- 
+
+  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
+  Version 0.1.13
+  Copyright (c)2015 Curious Solutions LLP and Neha Kadam
+  http://curioussolutions.github.io/DateTimePicker
+  https://github.com/CuriousSolutions/DateTimePicker
+
+ ----------------------------------------------------------------------------- */
+
+/*
+
+	language: Dutch
+	file: DateTimePicker-i18n-nl
+
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["nl"] = $.extend($.DateTimePicker.i18n["nl"], {
+        
+    	language: "nl",
+
+    	dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["zo", "ma", "di", "wo", "do", "vr", "za"],
+		fullDayNames: ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"],
+		shortMonthNames: ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+		fullMonthNames: ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+
+		titleContentDate: "Kies datum",
+		titleContentTime: "Kies tijd",
+		titleContentDateTime: "Kies datum & tijd",
+	
+		setButtonContent: "Kiezen",
+		clearButtonContent: "Leegmaken"
+        
+    });
+})(jQuery);
