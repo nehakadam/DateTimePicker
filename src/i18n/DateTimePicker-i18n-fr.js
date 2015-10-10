b7a4b8b795e8fa7ddf5d98ba74185ecb6896a362
Livio Gamassia <livio@lidmi.com>
2015-10-10 17:47:44 +0200

    Added French translation

---
+++ src/i18n/DateTimePicker-i18n-fr.js
@@ -0,0 +1,50 @@
+/* ----------------------------------------------------------------------------- 
+
+  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
+  Version 0.1.13
+  Copyright (c)2015 Curious Solutions LLP and Neha Kadam
+  http://curioussolutions.github.io/DateTimePicker
+  https://github.com/CuriousSolutions/DateTimePicker
+
+ ----------------------------------------------------------------------------- */
+
+/*
+
+	author: LivioGama
+	language: French
+	file: DateTimePicker-i18n-fr
+
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["fr"] = $.extend($.DateTimePicker.i18n["fr"], {
+        
+    	language: "fr",
+
+    	dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+		fullDayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+		shortMonthNames: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jun", "Jul", "Aoû", "Sep", "Oct", "Nov", "Déc"],
+		fullMonthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+
+		titleContentDate: "Choisir une date",
+		titleContentTime: "Choisir un horaire",
+		titleContentDateTime: "Choisir une date et un horaire",
+	
+		setButtonContent: "Choisir",
+		clearButtonContent: "Effacer",
+		formatHumanDate: function(oDate, sMode, sFormat)
+		{
+			if(sMode === "date")
+				return oDate.dayShort + " " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy;
+			else if(sMode === "time")
+				return oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+			else if(sMode === "datetime")
+				return oDate.dayShort + " " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy + ", " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+		}
+        
+    });
+})(jQuery);
