0d674c0000e411140f96d10fa5a8a8e3e19c168e
kristophone <kristophone@gmail.com>
2015-11-13 12:44:52 -0600

    i18n on spanish

---
+++ dist/i18n/DateTimePicker-i18n-es.js
@@ -0,0 +1,51 @@
+/* ----------------------------------------------------------------------------- 
+
+  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
+  Version 0.1.20
+  Copyright (c)2015 Curious Solutions LLP and Neha Kadam
+  http://curioussolutions.github.io/DateTimePicker
+  https://github.com/CuriousSolutions/DateTimePicker
+
+ ----------------------------------------------------------------------------- */
+
+/*
+
+	language: Spanish
+	file: DateTimePicker-i18n-es
+
+*/
+
+
+(function ($) {
+    $.DateTimePicker.i18n["es"] = $.extend($.DateTimePicker.i18n["es"], {
+        
+    	language: "es",
+
+    	dateTimeFormat: "dd-MMM-yyyy HH:mm:ss",
+		dateFormat: "dd-MMM-yyyy",
+		timeFormat: "HH:mm:ss",
+
+		shortDayNames: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
+		fullDayNames: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+	    shortMonthNames: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+	    fullMonthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+
+	    titleContentDate: "Ingresar fecha",
+		titleContentTime: "Ingresar hora",
+		titleContentDateTime: "Ingresar fecha y hora",
+	
+		setButtonContent: "Guardar",
+	    clearButtonContent: "Cancelar",
+
+		formatHumanDate: function(oDate, sMode, sFormat)
+		{
+			if(sMode === "date")
+				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ ", " + oDate.yyyy;
+			else if(sMode === "time")
+				return oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+			else if(sMode === "datetime")
+				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ ", " + oDate.yyyy + " " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+		}
+        
+    });
+})(jQuery);
