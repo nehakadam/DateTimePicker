eef9d797232fe4588b65247c6faa771ccea87a91
nehakadam <neha_kadam_2007@yahoo.co.in>
2015-11-28 16:52:58 +0530

    Fixed Bug and Added Example
    Fixed a bug caused when `settingValueOfElement(sValue, dDateTime,
    oInputElement) ` callback is not used.
    
    Added example to illustrate use of
    `setDateTimeStringInInputField(oInputField, dInput) ` to set datetime
    string in the input field.

--- src/DateTimePicker.js
+++ src/DateTimePicker.js
@@ -1,7 +1,7 @@
 /* ----------------------------------------------------------------------------- 
 
   jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
-  Version 0.1.23
+  Version 0.1.24
   Copyright (c)2015 Curious Solutions LLP and Neha Kadam
   http://curioussolutions.github.io/DateTimePicker
   https://github.com/CuriousSolutions/DateTimePicker
@@ -1651,7 +1651,8 @@ $.cf = {
 
 			var dElemValue = oDTP.getDateObjectForInputField($oElem);
 
-			oDTP.settings.settingValueOfElement.call(oDTP, sElemValue, dElemValue, $oElem);
+			if(oDTP.settings.settingValueOfElement)
+				oDTP.settings.settingValueOfElement.call(oDTP, sElemValue, dElemValue, $oElem);
 		
 			$oElem.change();		
 		
