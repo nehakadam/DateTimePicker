627513001c3b83b146e40bbb22b9a343257853db
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-01-27 18:24:15 +0200

    Add russian & ukrainian to i18n (build lang)

---
+++ dist/i18n/DateTimePicker-i18n-ro.js
@@ -0,0 +1,41 @@
+/* ----------------------------------------------------------------------------- 
+
+  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
+  Version 0.1.25
+  Copyright (c)2015 Curious Solutions LLP and Neha Kadam
+  http://curioussolutions.github.io/DateTimePicker
+  https://github.com/CuriousSolutions/DateTimePicker
+
+ ----------------------------------------------------------------------------- */
+
+/*
+
+	language: Romanian
+	file: DateTimePicker-i18n-nl
+	author: Radu Mogoș(https://github.com/pixelplant)
+
+ */
+
+(function ($) {
+	$.DateTimePicker.i18n["ro"] = $.extend($.DateTimePicker.i18n["ro"], {
+
+		language: "ro",
+
+		dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["Dum", "Lun", "Mar", "Mie", "Joi", "Vim", "Sâm"],
+		fullDayNames: ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă"],
+		shortMonthNames: ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Noi", "Dec"],
+		fullMonthNames: ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"],
+
+		titleContentDate: "Setare Dată",
+		titleContentTime: "Setare Oră",
+		titleContentDateTime: "Setare Dată și Oră",
+	
+		setButtonContent: "Setează",
+		clearButtonContent: "Șterge"
+
+	});
+})(jQuery);
--- src/i18n/DateTimePicker-i18n.js
+++ src/i18n/DateTimePicker-i18n.js
@@ -1,8 +1,8 @@
-/* -----------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------- 
 
   jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
   Version 0.1.25
-  Copyright (c)2015 Curious Solutions LLP and Neha Kadam
+  Copyright (c)2016 Curious Solutions LLP and Neha Kadam
   http://curioussolutions.github.io/DateTimePicker
   https://github.com/CuriousSolutions/DateTimePicker
 
@@ -17,7 +17,7 @@
 
 (function ($) {
     $.DateTimePicker.i18n["de"] = $.extend($.DateTimePicker.i18n["de"], {
-
+        
     	language: "de",
 
     	dateTimeFormat: "dd-MMM-yyyy HH:mm:ss",
@@ -32,7 +32,7 @@
 		titleContentDate: "Datum auswählen",
 		titleContentTime: "Zeit auswählen",
 		titleContentDateTime: "Datum & Zeit auswählen",
-
+	
 		setButtonContent: "Auswählen",
 		clearButtonContent: "Zurücksetzen",
 
@@ -45,7 +45,7 @@
 			else if(sMode === "datetime")
 				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ ", " + oDate.yyyy + " " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
 		}
-
+        
     });
 })(jQuery);
 
@@ -60,7 +60,7 @@
 
 (function ($) {
     $.DateTimePicker.i18n["en"] = $.extend($.DateTimePicker.i18n["en"], {
-
+        
     	language: "en",
 
     	dateTimeFormat: "dd-MM-yyyy HH:mm",
@@ -75,10 +75,10 @@
 		titleContentDate: "Set Date",
 		titleContentTime: "Set Time",
 		titleContentDateTime: "Set Date & Time",
-
+	
 		setButtonContent: "Set",
 		clearButtonContent: "Clear"
-
+        
     });
 })(jQuery);
 
@@ -95,7 +95,7 @@
 
 (function ($) {
     $.DateTimePicker.i18n["es"] = $.extend($.DateTimePicker.i18n["es"], {
-
+        
     	language: "es",
 
     	dateTimeFormat: "dd-MMM-yyyy HH:mm:ss",
@@ -110,7 +110,7 @@
 	    titleContentDate: "Ingresar fecha",
 		titleContentTime: "Ingresar hora",
 		titleContentDateTime: "Ingresar fecha y hora",
-
+	
 		setButtonContent: "Guardar",
 	    clearButtonContent: "Cancelar",
 
@@ -123,7 +123,7 @@
 			else if(sMode === "datetime")
 				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ ", " + oDate.yyyy + " " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
 		}
-
+        
     });
 })(jQuery);
 
@@ -140,7 +140,7 @@
 
 (function ($) {
     $.DateTimePicker.i18n["fr"] = $.extend($.DateTimePicker.i18n["fr"], {
-
+        
     	language: "fr",
 
     	dateTimeFormat: "dd-MM-yyyy HH:mm",
@@ -155,7 +155,7 @@
 		titleContentDate: "Choisir une date",
 		titleContentTime: "Choisir un horaire",
 		titleContentDateTime: "Choisir une date et un horaire",
-
+	
 		setButtonContent: "Choisir",
 		clearButtonContent: "Effacer",
 		formatHumanDate: function(oDate, sMode, sFormat)
@@ -167,9 +167,59 @@
 			else if(sMode === "datetime")
 				return oDate.dayShort + " " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy + ", " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
 		}
-
+        
     });
 })(jQuery);
+
+
+
+/*
+
+  language: Japanese
+  file: DateTimePicker-i18n-ja
+  author: JasonYCHuang (https://github.com/JasonYCHuang)
+
+*/
+
+(function ($) {
+   $.DateTimePicker.i18n["ja"] = $.extend($.DateTimePicker.i18n["ja"], {
+
+        language: "ja",
+        labels: {
+            'year': '年',
+            'month': '月',
+            'day': '日',
+            'hour': '時',
+            'minutes': '分',
+            'seconds': '秒',
+            'meridiem': '昼'
+        },
+        dateTimeFormat: "yyyy-MM-dd HH:mm",
+        dateFormat: "yyyy-MM-dd",
+        timeFormat: "HH:mm",
+
+        shortDayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+        fullDayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+        shortMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        fullMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+
+        titleContentDate: "日付の設定",
+        titleContentTime: "時刻の設定",
+        titleContentDateTime: "日付と時間の設定",
+
+        setButtonContent: "設定",
+        clearButtonContent: "取消",
+        formatHumanDate: function (oDate, sMode, sFormat) {
+            if (sMode === "date")
+                return  oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日";
+            else if (sMode === "time")
+                return oDate.HH + "時" + oDate.mm + "分" + oDate.ss + "秒";
+            else if (sMode === "datetime")
+                return oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日 " + oDate.HH + "時" + oDate.mm + "分";
+        }
+    });
+})(jQuery);
+
 
 
 
@@ -183,7 +233,7 @@
 
 (function ($) {
     $.DateTimePicker.i18n["nl"] = $.extend($.DateTimePicker.i18n["nl"], {
-
+        
     	language: "nl",
 
     	dateTimeFormat: "dd-MM-yyyy HH:mm",
@@ -198,10 +248,185 @@
 		titleContentDate: "Kies datum",
 		titleContentTime: "Kies tijd",
 		titleContentDateTime: "Kies datum & tijd",
-
+	
 		setButtonContent: "Kiezen",
 		clearButtonContent: "Leegmaken"
+        
+    });
+})(jQuery);
 
+
+
+
+/*
+
+	language: Romanian
+	file: DateTimePicker-i18n-nl
+	author: Radu Mogoș(https://github.com/pixelplant)
+
+ */
+
+(function ($) {
+	$.DateTimePicker.i18n["ro"] = $.extend($.DateTimePicker.i18n["ro"], {
+
+		language: "ro",
+
+		dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["Dum", "Lun", "Mar", "Mie", "Joi", "Vim", "Sâm"],
+		fullDayNames: ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă"],
+		shortMonthNames: ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Noi", "Dec"],
+		fullMonthNames: ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"],
+
+		titleContentDate: "Setare Dată",
+		titleContentTime: "Setare Oră",
+		titleContentDateTime: "Setare Dată și Oră",
+	
+		setButtonContent: "Setează",
+		clearButtonContent: "Șterge"
+
+	});
+})(jQuery);
+
+
+
+/*
+
+  language: Russian
+  file: DateTimePicker-i18n-ru
+  author: Valery Bogdanov (https://github.com/radkill)
+
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["ru"] = $.extend($.DateTimePicker.i18n["ru"], {
+
+    language: "ru",
+
+    dateTimeFormat: "dd-MM-yyyy HH:mm",
+    dateFormat: "dd-MM-yyyy",
+    timeFormat: "HH:mm",
+
+    shortDayNames: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    fullDayNames: ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"],
+    shortMonthNames: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+    fullMonthNames: ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"],
+
+    titleContentDate: "Выберите дату",
+    titleContentTime: "Выберите время",
+    titleContentDateTime: "Выберите дату и время",
+
+    setButtonContent: "Выбрать",
+    clearButtonContent: "Очистить",
+
+    formatHumanDate: function(oDate, sMode, sFormat)
+		{
+			if(sMode === "date")
+				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy;
+			else if(sMode === "time")
+				return oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+			else if(sMode === "datetime")
+				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy + ", " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+		}
+
+    });
+})(jQuery);
+
+
+
+/*
+
+  language: Ukrainian
+  file: DateTimePicker-i18n-uk
+  author: Valery Bogdanov (https://github.com/radkill)
+
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["uk"] = $.extend($.DateTimePicker.i18n["uk"], {
+
+    language: "uk",
+
+    dateTimeFormat: "dd-MM-yyyy HH:mm",
+    dateFormat: "dd-MM-yyyy",
+    timeFormat: "HH:mm",
+
+    shortDayNames: ["Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    fullDayNames: ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"],
+    shortMonthNames: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+    fullMonthNames: ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"],
+    fullDayNames: ["неділя", "понеділок", "вівторок", "середа", "четвер", "п'ятниця", "субота"],
+    shortMonthNames: ["Січ", "Лют", "Бер", "Кві", "Тра", "Чер", "Лип", "Сер", "Вер", "Жов", "Лис", "Гру"],
+    fullMonthNames: ["січня", "лютого", "березня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня"],
+
+    titleContentDate: "Виберіть дату",
+    titleContentTime: "Виберіть час",
+    titleContentDateTime: "Виберіть дату і час",
+
+    setButtonContent: "Вибрати",
+    clearButtonContent: "Очистити",
+
+    formatHumanDate: function(oDate, sMode, sFormat)
+		{
+			if(sMode === "date")
+				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy;
+			else if(sMode === "time")
+				return oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+			else if(sMode === "datetime")
+				return oDate.dayShort + ", " + oDate.dd + " " + oDate.month+ " " + oDate.yyyy + ", " + oDate.HH + ":" + oDate.mm + ":" + oDate.ss;
+		}
+
+    });
+})(jQuery);
+
+
+
+/*
+
+  language: Traditional Chinese
+  file: DateTimePicker-i18n-zh-TW
+  author: JasonYCHuang (https://github.com/JasonYCHuang)
+
+*/
+
+(function ($) {
+   $.DateTimePicker.i18n["zh-TW"] = $.extend($.DateTimePicker.i18n["zh-TW"], {
+
+        language: "zh-TW",
+        labels: {
+            'year': '年',
+            'month': '月',
+            'day': '日',
+            'hour': '時',
+            'minutes': '分',
+            'seconds': '秒',
+            'meridiem': '午'
+        },
+        dateTimeFormat: "yyyy-MM-dd HH:mm",
+        dateFormat: "yyyy-MM-dd",
+        timeFormat: "HH:mm",
+
+        shortDayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+        fullDayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+        shortMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        fullMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+
+        titleContentDate: "設置日期",
+        titleContentTime: "設置時間",
+        titleContentDateTime: "設置日期和時間",
+
+        setButtonContent: "設置",
+        clearButtonContent: "清除",
+        formatHumanDate: function (oDate, sMode, sFormat) {
+            if (sMode === "date")
+                return  oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日";
+            else if (sMode === "time")
+                return oDate.HH + "時" + oDate.mm + "分" + oDate.ss + "秒";
+            else if (sMode === "datetime")
+                return oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日 " + oDate.HH + "時" + oDate.mm + "分";
+        }
     });
 })(jQuery);
 
@@ -251,133 +476,6 @@
                 return oDate.HH + "时" + oDate.mm + "分" + oDate.ss + "秒";
             else if (sMode === "datetime")
                 return oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日 " + oDate.HH + "时" + oDate.mm + "分";
-        }
-    });
-})(jQuery);
-
-/*
-
- language: Romanian
- file: DateTimePicker-i18n-nl
- author: Radu Mogoș(https://github.com/pixelplant)
-
- */
-
-(function ($) {
-	$.DateTimePicker.i18n["ro"] = $.extend($.DateTimePicker.i18n["ro"], {
-
-		language: "ro",
-
-		dateTimeFormat: "dd-MM-yyyy HH:mm",
-		dateFormat: "dd-MM-yyyy",
-		timeFormat: "HH:mm",
-
-		shortDayNames: ["Dum", "Lun", "Mar", "Mie", "Joi", "Vim", "Sâm"],
-		fullDayNames: ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă"],
-		shortMonthNames: ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Noi", "Dec"],
-		fullMonthNames: ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"],
-
-		titleContentDate: "Setare Dată",
-		titleContentTime: "Setare Oră",
-		titleContentDateTime: "Setare Dată și Oră",
-
-		setButtonContent: "Setează",
-		clearButtonContent: "Șterge"
-
-	});
-})(jQuery);
-
-
-/*
-
-	language: Traditional Chinese
-	file: DateTimePicker-i18n-zh-TW
-	author: JasonYCHuang(https://github.com/JasonYCHuang)
-
-*/
-
-(function ($) {
-   $.DateTimePicker.i18n["zh-TW"] = $.extend($.DateTimePicker.i18n["zh-TW"], {
-
-        language: "zh-TW",
-        labels: {
-            'year': '年',
-            'month': '月',
-            'day': '日',
-            'hour': '時',
-            'minutes': '分',
-            'seconds': '秒',
-            'meridiem': '午'
-        },
-        dateTimeFormat: "yyyy-MM-dd HH:mm",
-        dateFormat: "yyyy-MM-dd",
-        timeFormat: "HH:mm",
-
-        shortDayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
-        fullDayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
-        shortMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-        fullMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-
-        titleContentDate: "設置日期",
-        titleContentTime: "設置時間",
-        titleContentDateTime: "設置日期和時間",
-
-        setButtonContent: "設置",
-        clearButtonContent: "清除",
-        formatHumanDate: function (oDate, sMode, sFormat) {
-            if (sMode === "date")
-                return  oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日";
-            else if (sMode === "time")
-                return oDate.HH + "時" + oDate.mm + "分" + oDate.ss + "秒";
-            else if (sMode === "datetime")
-                return oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日 " + oDate.HH + "時" + oDate.mm + "分";
-        }
-    });
-})(jQuery);
-
-/*
-
-  language: Japanese
-  file: DateTimePicker-i18n-ja
-  author: JasonYCHuang (https://github.com/JasonYCHuang)
-
-*/
-
-(function ($) {
-   $.DateTimePicker.i18n["ja"] = $.extend($.DateTimePicker.i18n["ja"], {
-
-        language: "ja",
-        labels: {
-            'year': '年',
-            'month': '月',
-            'day': '日',
-            'hour': '時',
-            'minutes': '分',
-            'seconds': '秒',
-            'meridiem': '昼'
-        },
-        dateTimeFormat: "yyyy-MM-dd HH:mm",
-        dateFormat: "yyyy-MM-dd",
-        timeFormat: "HH:mm",
-
-        shortDayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
-        fullDayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
-        shortMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-        fullMonthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-
-        titleContentDate: "日付の設定",
-        titleContentTime: "時刻の設定",
-        titleContentDateTime: "日付と時間の設定",
-
-        setButtonContent: "設定",
-        clearButtonContent: "取消",
-        formatHumanDate: function (oDate, sMode, sFormat) {
-            if (sMode === "date")
-                return  oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日";
-            else if (sMode === "time")
-                return oDate.HH + "時" + oDate.mm + "分" + oDate.ss + "秒";
-            else if (sMode === "datetime")
-                return oDate.dayShort + ", " + oDate.yyyy + "年" +  oDate.month +"月" + oDate.dd + "日 " + oDate.HH + "時" + oDate.mm + "分";
         }
     });
 })(jQuery);
