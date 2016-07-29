767db65585124f5558f9b0e54470dea54f6a8376
yanike <yanikegroup@yahoo.com>
2016-07-29 16:43:02 -0500

    Update DateTimePicker_iOS_fix.js

--- src/DateTimePicker_iOS_fix.js
+++ src/DateTimePicker_iOS_fix.js
@@ -8,36 +8,42 @@ https://github.com/CuriousSolutions/DateTimePicker
 
 ----------------------------------------------------------------------------- */
 
-function dtpickerContMoveiOS() {
-	/* Detect iOS device */
-	var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+/* Detect iOS device */
+var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-	/* Execute if iOS device is found  */
-	if (iOS) {
-		/* Get SCROLL location */
-		var tscroll = window.scrollY /*Modern Way (Chrome, Firefox)*/ || window.pageYOffset /*Modern IE, including IE11*/ || document.documentElement.scrollTop /*Old IE, 6,7,8*/ ;
-		/* Calculate position */
-		var topscroll = (((document.documentElement.clientHeight / 2) + tscroll) + "px");
-		/* Make POSITION ABSOLUTE so iOS devices can interact with DateTimePicker */
-		jQuery('.dtpicker-mobile').css('position', 'absolute');
-		jQuery('.dtpicker-cont').css('position', 'absolute');
-		/* Scroll DateTimePicker into position */
-		jQuery('.dtpicker-cont').css('top', topscroll);
+/* Execute if iOS device is found  */
+if (iOS) {
+	function dtpickerContMoveiOS() {
+		/* Execute if iOS device is found  */
+		if (iOS) {
+			/* Get SCROLL location */
+			var tscroll = window.scrollY /*Modern Way (Chrome, Firefox)*/ || window.pageYOffset /*Modern IE, including IE11*/ || document.documentElement.scrollTop /*Old IE, 6,7,8*/ ;
+			/* Calculate position */
+			var topscroll = (((document.documentElement.clientHeight / 2) + tscroll) + "px");
+			/* Make POSITION ABSOLUTE so iOS devices can interact with DateTimePicker */
+			jQuery('.dtpicker-mobile').css('position', 'absolute');
+			jQuery('.dtpicker-cont').css('position', 'absolute');
+			/* Scroll DateTimePicker into position */
+			jQuery('.dtpicker-cont').css('top', topscroll);
+		}
 	}
-}
 
-/* WINDOW EVENTLISTENER to SCROLL */
-if (window.addEventListener) {
-	window.addEventListener('scroll', dtpickerContMoveiOS, false);
-} else if (window.attachEvent) {
-	window.attachEvent('on' + 'scroll', dtpickerContMoveiOS);
-} else {
-	window['on' + 'scroll'] = dtpickerContMoveiOS;
-}
+	/* WINDOW EVENTLISTENER to LOAD and SCROLL */
+	if (window.addEventListener) {
+		window.addEventListener('load', dtpickerContMoveiOS, false);
+		window.addEventListener('scroll', dtpickerContMoveiOS, false);
+	} else if (window.attachEvent) {
+		window.attachEvent('on' + 'load', dtpickerContMoveiOS);
+		window.attachEvent('on' + 'scroll', dtpickerContMoveiOS);
+	} else {
+		window['on' + 'load'] = dtpickerContMoveiOS;
+		window['on' + 'scroll'] = dtpickerContMoveiOS;
+	}
 
-/* Makes readonly INPUTS on iOS correctly work */
-$(function() {
-	$('input[readonly]').on('focus', function(ev) {
-		$(this).trigger('blur');
+	/* Makes readonly INPUTS on iOS correctly work */
+	$(function() {
+		$('input[readonly]').on('focus', function(ev) {
+			$(this).trigger('blur');
+		});
 	});
-});
+}
