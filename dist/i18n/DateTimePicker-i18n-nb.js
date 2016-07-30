7ae1c2cc0c42ccc8ae57445eb8f5cef36fa283dd
Neha Kadam <nehakadam9@gmail.com>
2016-07-30 19:57:18 +0530

    Inline View, Incr/Decr with Arrow Keys and Mousewheel
    DateTimePicker can be added as an inline element by setting `isInline:
    true`. Since DateTimePicker is added inline, input element should be
    explicitly specified with `inputElement: <jQuery object of Element>`.
    Check Example [Inline View](demo/View-Inline.htm).
    
    Input Elements on DateTimePicker can be made read-only with
    `readonlyInputs: true`.
    
    Header of DateTimePicker Element can be removed with `showHeader:
    false`.
    
    Added functionality to increment or decrement values of dates using Up
    and Down Arrow Keys as well as using Mouse Wheel.

--- dist/i18n/DateTimePicker-i18n-nb.js
+++ dist/i18n/DateTimePicker-i18n-nb.js
@@ -1,7 +1,7 @@
 /* -----------------------------------------------------------------------------
 
  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
- Version 0.1.30
+ Version 0.1.31
  Copyright (c)2016 Curious Solutions LLP and Neha Kadam
  http://curioussolutions.github.io/DateTimePicker
  https://github.com/CuriousSolutions/DateTimePicker
