0d63b53dcdbfa5669dca06388032b459ad58ee2b
jirka <forstjiri@gmail.com>
2016-09-09 09:21:47 +0200

    Added Czech (cs) localization

--- dist/i18n/DateTimePicker-i18n.js
+++ dist/i18n/DateTimePicker-i18n.js
@@ -10,6 +10,40 @@
 
 /*
 
+	language: Czech
+	file: DateTimePicker-i18n-cs
+  	author: aiphee (https://github.com/aiphee)
+
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["cs"] = $.extend($.DateTimePicker.i18n["cs"], {
+
+    	language: "cs",
+
+    	dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["Ne", "Po", "Út", "St", "Čt", "Pá", "So"],
+		fullDayNames: ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"],
+		shortMonthNames: ["Led", "Úno", "Bře", "Dub", "Kvě", "Čer",  "čvc", "Srp", "Zář", "Říj", "Lis", "Pro"],
+		fullMonthNames: ["Leden", "Únor", "Březen", "Duben", "Květen", "Červen",  "červenec", "Srpen", "Září", "Říjen", "Listopad", "Prosinec"],
+
+		titleContentDate: "Nastavit datum",
+		titleContentTime: "Nastavit čas",
+		titleContentDateTime: "Nastavit datum a čas",
+
+		setButtonContent: "Nastavit",
+		clearButtonContent: "Resetovat"
+
+    });
+})(jQuery);
+
+
+
+/*
+
 	language: German
 	file: DateTimePicker-i18n-de
 	author: Lu, Feng (https://github.com/solala888)
