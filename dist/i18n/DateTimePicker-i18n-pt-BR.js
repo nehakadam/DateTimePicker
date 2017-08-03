726a701869050fd0165051b4234b12d043bb5ec6
Bruno Rodrigues <brunocroh@gmail.com>
2017-08-03 20:10:55 -0300

    solve idention problem

--- dist/i18n/DateTimePicker-i18n-pt-BR.js
+++ dist/i18n/DateTimePicker-i18n-pt-BR.js
@@ -19,23 +19,23 @@
 (function ($) {
     $.DateTimePicker.i18n["pt-br"] = $.extend($.DateTimePicker.i18n["pt-br"], {
 
-    	language: "pt-br",
+        language: "pt-br",
 
-    	dateTimeFormat: "dd-MM-yyyy HH:mm",
-		dateFormat: "dd-MM-yyyy",
-		timeFormat: "HH:mm",
+        dateTimeFormat: "dd-MM-yyyy HH:mm",
+        dateFormat: "dd-MM-yyyy",
+        timeFormat: "HH:mm",
 
-		shortDayNames: ["Dom", "Seg", "Terç", "Qua", "Qui", "Sex", "Sáb"],
-		fullDayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
-		shortMonthNames: ["jan", "fev", "mar", "abr", "maio", "jun", "jul", "ago", "set", "out", "nov", "dez"],
-		fullMonthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro",],
+        shortDayNames: ["Dom", "Seg", "Terç", "Qua", "Qui", "Sex", "Sáb"],
+        fullDayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+        shortMonthNames: ["jan", "fev", "mar", "abr", "maio", "jun", "jul", "ago", "set", "out", "nov", "dez"],
+        fullMonthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro",],
 
-		titleContentDate: "Definir Data",
-		titleContentTime: "Definir Hora",
-		titleContentDateTime: "Definir Data e Hora",
-	
-		setButtonContent: "Definir",
-		clearButtonContent: "Limpar"
+        titleContentDate: "Definir Data",
+        titleContentTime: "Definir Hora",
+        titleContentDateTime: "Definir Data e Hora",
+
+        setButtonContent: "Definir",
+        clearButtonContent: "Limpar"
 
     });
 })(jQuery);
