ccce9567af2ea8b0a5bc14ca097bb3f584e29d88
Bruno Rodrigues <Brunocroh@users.noreply.github.com>
2017-08-03 21:51:30 -0300

    Update DateTimePicker-i18n-pt-BR.js

--- dist/i18n/DateTimePicker-i18n-pt-BR.js
+++ dist/i18n/DateTimePicker-i18n-pt-BR.js
@@ -28,7 +28,7 @@
         shortDayNames: ["Dom", "Seg", "Terç", "Qua", "Qui", "Sex", "Sáb"],
         fullDayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
         shortMonthNames: ["jan", "fev", "mar", "abr", "maio", "jun", "jul", "ago", "set", "out", "nov", "dez"],
-        fullMonthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro",],
+        fullMonthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"],
 
         titleContentDate: "Definir Data",
         titleContentTime: "Definir Hora",
